c5d0ffa8bb5ded64961bfdecb13ac78aedf999e1
artyombatura <artyombatura@gmail.com>
2020-02-14 21:31:22 +0300

    TableView UI Added

---
+++ BalinaSoftTestApp/Model/DeveloperModel.swift
@@ -0,0 +1,19 @@
+//
+//  DeveloperModel.swift
+//  BalinaSoftTestApp
+//
+//  Created by Artyom on 2/14/20.
+//  Copyright © 2020 Artyom. All rights reserved.
+//
+
+import Foundation
+
+class Developer {
+    
+    public var name: String
+    public var id: String
+    
+    init(json: [String:Any]) {
+        
+    }
+}
---
+++ BalinaSoftTestApp/View/InfoTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  InfoTableViewCell.swift
+//  BalinaSoftTestApp
+//
+//  Created by Artyom on 2/14/20.
+//  Copyright © 2020 Artyom. All rights reserved.
+//
+
+import UIKit
+
+class InfoTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var idLable: UILabel!
+    
+}
