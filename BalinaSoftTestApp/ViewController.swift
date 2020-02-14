5b8a9d769dfe1af514bdb1c253bd51a480c5d528
artyombatura <artyombatura@gmail.com>
2020-02-14 21:16:22 +0300

    Initial commit

---
+++ BalinaSoftTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BalinaSoftTestApp
+//
+//  Created by Artyom on 2/14/20.
+//  Copyright © 2020 Artyom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
